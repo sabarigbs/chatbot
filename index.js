508e98adc72c0573979437c435eddaa011df1edd
sabarigbs <sabarigovindarajan@gmail.com>
2018-02-25 15:44:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var server = express();
+
+server.use(bodyParser.json());
+
+server.use(bodyParser.urlencoded({
+    extended : false
+}));
+
+server.post('/',function(req,res,next){
+
+    var dataToSend = req.body.message;
+    console.log(dataToSend);
+
+    return res.json({
+        speech : dataToSend,
+        displayText : dataToSend,
+        source : '/'
+    });
+
+});
+
+server.listen((process.env.PORT || 8000), function () {
+    console.log("Server is up and running...");
+});
