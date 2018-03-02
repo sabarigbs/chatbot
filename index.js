6a3dfbc28563e9139d13096cf728c56907ef6f34
Sabari <sabarigovindarajan@gmail.com>
2018-03-02 21:11:21 +0530

    Update index.js
    small

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.use(bodyParser.urlencoded({
 }));
 
 server.post('/',function(req,res,next){
-
+    console.log(req.body);
     var dataToSend = req.body.parameters;
     console.log("This is test from server "+dataToSend);
 
