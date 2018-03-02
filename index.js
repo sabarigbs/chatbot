2a1ffbc79cea2258f8ab2c79bdf723b32c49da72
Sabari <sabarigovindarajan@gmail.com>
2018-03-02 21:24:43 +0530

    Update index.js
    json

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ server.post('/',function(req,res,next){
     console.log(req.body.queryResult.queryText);
     var dataToSend = req.body.queryResult.queryText;
     console.log("This is test from server "+dataToSend);
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.writeHead(200, { 'Content-Type': 'text/json' })
     res.json({
         speech : dataToSend,
         displayText : dataToSend,
