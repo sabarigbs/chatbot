228ea62d5270339f6f3e5b10820e45761fcefbc9
sabarigbs <sabarigovindarajan@gmail.com>
2018-03-03 20:14:20 +0530

    V@ Dropped

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ server.use(bodyParser.urlencoded({
 }));
 
 server.post('/',function(req,res,next){
-    console.log(req.body.result);
-    var dataToSend = req.body.result;
+    console.log(req.body.result.parameters);
+    var dataToSend = req.body.result.parameters;
     console.log("This is test from server "+dataToSend);
     res.setHeader('Content-Type', 'application/json');
     return res.json({
